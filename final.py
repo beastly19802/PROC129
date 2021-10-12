fda87a8e87bd8e8995382828251c2544038a2f83
beastly19802 <69489254+beastly19802@users.noreply.github.com>
2021-10-11 21:03:19 -0400

    Add files via upload

---
+++ final.py
@@ -0,0 +1,124 @@
+import csv
+import pandas as pd
+
+file1 = 'bright_stars.csv'
+file2 = 'unit_converted_stars.csv'
+
+d1 = []
+d2 = []
+with open(file1,'r',encoding='utf8') as f:
+    csv_reader =csv.reader(f)
+    
+    for i in csv_reader:
+        d1.append(i)
+        
+with open(file2,'r',encoding='utf8') as f:
+    csv_reader = csv.reader(f)
+    
+    for i in csv_reader:
+        d2.append(i)
+
+h1 = d1[0]
+h2 = d2[0]
+
+p_d1 = d1[1:]
+p_d2 = d2[1:]
+
+h = h1+h2
+
+p_d =[]
+
+for i in p_d1:
+    p_d.append(i)
+for j in p_d2:
+    p_d.append(j)
+with open("total_stars.csv",'w',encoding='utf8') as f:
+    csvwriter = csv.writer(f)
+    csvwriter.writerow(h)   
+    csvwriter.writerows(p_d)
+    
+df = pd.read_csv('total_stars.csv')
+df.tail(8)
+from bs4 import BeautifulSoup as bs
+import requests
+import pandas as pd
+
+
+bright_stars_url = 'https://en.wikipedia.org/wiki/List_of_brightest_stars_and_other_record_stars'
+
+page = requests.get(bright_stars_url)
+print(page)
+
+soup = bs(page.text,'html.parser')
+
+star_table = soup.find('table')
+
+temp_list= []
+table_rows = star_table.find_all('tr')
+for tr in table_rows:
+    td = tr.find_all('td')
+    row = [i.text.rstrip() for i in td]
+    temp_list.append(row)
+
+
+
+Star_names = []
+Distance =[]
+Mass = []
+Radius =[]
+Lum = []
+
+for i in range(1,len(temp_list)):
+    Star_names.append(temp_list[i][1])
+    Distance.append(temp_list[i][3])
+    Mass.append(temp_list[i][5])
+    Radius.append(temp_list[i][6])
+    Lum.append(temp_list[i][7])
+    
+df2 = pd.DataFrame(list(zip(Star_names,Distance,Mass,Radius,Lum)),columns=['Star_name','Distance','Mass','Radius','Luminosity'])
+print(df2)
+
+df2.to_csv('bright_stars.csv')
+from bs4 import BeautifulSoup as bs
+import requests
+import pandas as pd
+
+
+url = 'https://en.wikipedia.org/wiki/List_of_brown_dwarfs'
+
+page = requests.get(url)
+print(page)
+
+soup = bs(page.text,'html.parser')
+
+star_table = soup.find_all('table')
+print(len(star_table))
+
+
+temp_list= []
+table_rows = star_table[4].find_all('tr')
+for tr in table_rows:
+    td = tr.find_all('td')
+    row = [i.text.rstrip() for i in td]
+    temp_list.append(row)
+print(temp_list)
+
+
+
+Star_names = []
+Distance =[]
+Mass = []
+Radius =[]
+
+
+for i in range(1,len(temp_list)):
+    
+    Star_names.append(temp_list[i][0])
+    Distance.append(temp_list[i][5])
+    Mass.append(temp_list[i][7])
+    Radius.append(temp_list[i][8])
+
+df2 = pd.DataFrame(list(zip(Star_names,Distance,Mass,Radius,)),columns=['Star_name','Distance','Mass','Radius'])
+print(df2)
+
+df2.to_csv('dwarf_stars.csv')
